c5523526194e03f4ef39edb353b6c7bc1bcbbba1
s241008 <s241008@kashiwanoha.ed.jp>
2025-10-23 14:05:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+})*/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // プロジェクトのルートを指定（基本的にはこれでOK）
+  publicDir: 'public', // publicフォルダの明示
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // srcを@で参照可能に
+    },
+  },
 })
