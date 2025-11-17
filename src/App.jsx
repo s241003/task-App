f2bfb4a43635a84bb415149bdfd2b2a522e37303
kenta <kenta.uruno@gmail.com>
2025-11-17 23:11:47 +0900

    1117 vercel.json-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { supabase } from "../components/AI/AITaskColl";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from "react-router-dom";
 import './App.css';
 import NavigationBar from '../components/Function/NavigationBar';
 import LoginPage from '../components/Function/Pages/LoginPage';
@@ -21,6 +21,7 @@ export const formatDateDisplay = (date) => {
   const day = date.getDate();
   return `${year}年${month}月${day}日`;
 };
+
 
 export const NotFound = () =>{
   return(
@@ -66,6 +67,13 @@ const App = () => {
       fetchTasks()
     }, [])
 
+
+    const location = useLocation();
+    const currentPage = location.pathname;
+    const validPaths = ["/", "/tasks", "/calendar", "/groupwork"];
+
+    const isNotFound = !validPaths.includes(currentPath);
+
   return (
 
     // ルーティング
@@ -78,7 +86,7 @@ const App = () => {
           <Route path="/groupwork" element={<CalendarPage tasks={tasks} setTasks={setTasks} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <NavigationBar />
+        {(isNotFound) ? <NavigationBar />: null}
       </Router>
     </div>
   );
