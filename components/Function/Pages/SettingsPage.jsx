a4512c11aa617947d4ec961ac9035b6831c009f0
s241008 <s241008@kashiwanoha.ed.jp>
2026-01-13 10:33:21 +0900

    Update SettingsPage.jsx

--- components/Function/Pages/SettingsPage.jsx
+++ components/Function/Pages/SettingsPage.jsx
@@ -1,1 +1,24 @@
+function lightMode() {
+  const lightClick = () => {
+    alert("押されました！");
+  };
+
+  return (
+    <button className="lightModeButton" onClick={lightClick}>
+      ライトモード
+    </button>
+  );
+}
+
+function darkMode() {
+  const darkClick = () => {
+    alert("押されました！");
+  };
+
+  return (
+    <button className="darkModeButton" onClick={darkClick}>
+      ダークモード
+    </button>
+  );
+}
 
