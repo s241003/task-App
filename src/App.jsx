206045a553ed13f03f3cbbe18e6df2a05c7a5aea
s241008 <s241008@kashiwanoha.ed.jp>
2026-01-15 13:41:27 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -107,7 +107,7 @@ const App = () => {
           <Route path="/addTask" element={<AITaskColl onTaskCreated={handleAddTaskFromAI} />} />
           <Route path="/aichat" element={<AIChat />} />
           <Route path="/groupwork" element={<CalendarPage tasks={tasks} setTasks={setTasks} />} />
-          <Route path="/setting" element={<SettingPage />} />
+          <Route path="/setting" element={<SettingsPage />} />
           <Route path="*" element={<NotFound setIsNotFound={setIsNotFound} />} />
         </Routes>
         {(!isNotFound) ? <NavigationBar />: null}
